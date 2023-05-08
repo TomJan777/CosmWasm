d75990d3d92b0378e74f5277682ad7a8fa26f707
Tomasz <cryptodev77@gmail.com>
2023-05-08 21:07:00 +0200

    First smart contract, all in one file, compiles

--- cw-academy/empty-contract/src/lib.rs
+++ cw-academy/empty-contract/src/lib.rs
@@ -13,9 +13,28 @@ mod tests {
     }
 }*/
 
+
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct QueryResp {
+    message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum QueryMsg {
+    Greet{},
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GreetResp {
+    message: String,
+}
+
 
 #[entry_point]
 pub fn instantiate(
@@ -26,3 +45,27 @@ pub fn instantiate(
 ) -> StdResult<Response> {
     Ok(Response::new())
 }
+
+pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    use QueryMsg::*;
+
+    match msg {
+        Greet {} => to_binary(&query::greet()?),
+    }    
+}
+
+mod query {
+    use super::*;
+
+    pub fn greet() -> StdResult<GreetResp> {
+        let resp = GreetResp {
+            message: "Hello World".to_owned(),
+        };
+
+        Ok(resp)
+    }
+}
+
+
+
+
